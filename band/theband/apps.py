18c1a198049b645c5ccdd7d3191d71a57d5d432c
Clever <clevermabunda@gmail.com>
2024-10-28 17:21:14 +0200

    added docstrings to theband/apps.py

--- band/theband/apps.py
+++ band/theband/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
 
-
 class ThebandConfig(AppConfig):
+    """
+    Configuration for the 'theband' application.
+
+    This class is used to configure the 'theband' Django app,
+    specifying the name and the default auto field type for models.
+    """
+    
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'theband'
